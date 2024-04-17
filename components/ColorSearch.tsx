d106e9bdb61befbd0582c1cca7762085490bb556
Joacim Sørlle <kiragutt@hotmail.com>
2024-04-17 10:20:47 +0200

    La til Lukk knapp og tilbakestill knapp
    Design for filter + tilbakestill og lukk knapp

--- components/ColorSearch.tsx
+++ components/ColorSearch.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import algoliasearch from 'algoliasearch/lite';
 import 'instantsearch.css/themes/satellite.css';
-import {Configure, InstantSearch, SearchBox, RefinementList, CurrentRefinements, connectStateResults} from 'react-instantsearch-dom';
+import {Configure, InstantSearch, SearchBox, RefinementList, CurrentRefinements, connectStateResults, ClearRefinements} from 'react-instantsearch-dom';
 import {HitProps} from "@/components/ColorSearchHit";
 import {SearchResults} from "algoliasearch-helper";
 import {CurrentRefinementsProps} from "react-instantsearch";
@@ -67,24 +67,30 @@ export const Search: React.FC<SearchProps> = ({ onResultsUpdate }) => {
         <InstantSearch searchClient={searchClient} indexName="colours_dump">
             <Configure hitsPerPage={500}/>
             <CustomResults onResultsUpdate={onResultsUpdate}/>
-            <CurrentRefinements transformItems={transformItems} />
+            <CurrentRefinements transformItems={transformItems}/>
 
             <div className="grid grid-cols-3 gap-2">
                 <div className="col-span-2">
-                    <SearchBox translations={{ placeholder: 'Søk her' }}/>
+                    <SearchBox translations={{placeholder: 'Søk her'}}/>
                 </div>
 
                 <button onClick={toggleCollapse}
                         className="text-sm lg:text-xs xl:text-sm border-2 bg-white hover:border-gray-500 rounded-lg">
-                    {isCollapsed ? 'Vis filter' : 'Gjem filter'}
+                    {isCollapsed ? 'Vis filter' : 'Skjul filter'}
                 </button>
             </div>
 
             {/* Slideout panel for the filters */}
             <div className={`filter-panel ${!isCollapsed ? 'active' : ''}`}>
-                <div className={isCollapsed ? 'hidden' : ''}>
-                    <RefinementList attribute="collections.name"/>
-                </div>
+                <ClearRefinements
+                    translations={{
+                        reset: 'Tilbakestill alle filtre'
+                    }}
+                />
+                <RefinementList attribute="collections.name"/>
+                <button onClick={() => setIsCollapsed(true)} className="reset-button">
+                    Lukk
+                </button>
             </div>
         </InstantSearch>
     );
