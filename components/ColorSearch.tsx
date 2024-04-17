e7cc82a5e23d936e8fa0a38a38ceb9ad16f3bbd5
Joacim Sørlle <kiragutt@hotmail.com>
2024-04-17 08:45:01 +0200

    Satt opp filter slideout og filtertags, må designes

--- components/ColorSearch.tsx
+++ components/ColorSearch.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import algoliasearch from 'algoliasearch/lite';
 import 'instantsearch.css/themes/satellite.css';
-import {Configure, InstantSearch, SearchBox, RefinementList, connectStateResults} from 'react-instantsearch-dom';
+import {Configure, InstantSearch, SearchBox, RefinementList, CurrentRefinements, connectStateResults} from 'react-instantsearch-dom';
 import {HitProps} from "@/components/ColorSearchHit";
 import {SearchResults} from "algoliasearch-helper";
+import {CurrentRefinementsProps} from "react-instantsearch";
 
 interface CustomResultsProps{
     searchState: any;
@@ -30,7 +31,12 @@ const searchClient = algoliasearch('NOLK3JAMLX', 'fcde24d65b04aa23920ceb878b4362
 interface SearchProps {
     onResultsUpdate: (hits: HitProps[]) => void;
 }
-
+const transformItems: CurrentRefinementsProps['transformItems'] = (items) => {
+    return items.map(item => ({
+        ...item,
+        label: item.label.replace(/^[^:]+: /, '')  // Removes everything before and after the ":".
+    }));
+};
 export const Search: React.FC<SearchProps> = ({ onResultsUpdate }) => {
     const [isCollapsed, setIsCollapsed] = useState(true); // Default to collapsed
 
@@ -39,24 +45,25 @@ export const Search: React.FC<SearchProps> = ({ onResultsUpdate }) => {
         <InstantSearch searchClient={searchClient} indexName="colours_dump">
             <Configure hitsPerPage={500}/>
             <CustomResults onResultsUpdate={onResultsUpdate}/>
+            <CurrentRefinements transformItems={transformItems} />
 
             <div className="grid grid-cols-4 gap-2">
                 <div className="col-span-3">
                     <SearchBox translations={{ placeholder: 'Søk her' }}/>
                 </div>
 
-                <button onClick={toggleCollapse} className="text-sm lg:text-xs xl:text-sm border-2 bg-white hover:border-gray-500 rounded-lg">
+                <button onClick={toggleCollapse}
+                        className="text-sm lg:text-xs xl:text-sm border-2 bg-white hover:border-gray-500 rounded-lg">
                     {isCollapsed ? 'Vis filter' : 'Gjem filter'}
                 </button>
-
-                {/* Always render the RefinementList, but control its visibility with a CSS class */}
-                <div className="col-span-3">
-                    <div className={isCollapsed ? 'hidden' : ''}>
-                        <RefinementList attribute="collections.name"/>
-                    </div>
-                </div>
             </div>
 
+            {/* Slideout panel for the filters */}
+            <div className={`filter-panel ${!isCollapsed ? 'active' : ''}`}>
+                <div className={isCollapsed ? 'hidden' : ''}>
+                    <RefinementList attribute="collections.name"/>
+                </div>
+            </div>
         </InstantSearch>
     );
 };
