6d35872faefd3e27ae6c574855262cf693a896a5
Caleb <89772184+belacmu@users.noreply.github.com>
2024-04-19 14:44:15 +0200

    change html lang to no

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="no">
             <Script id="clarity-script" strategy="afterInteractive">
                 {`
                     (function(c,l,a,r,i,t,y){
