b9d1ce2fb6815d8ae2a7b8fe744ea4f676a3ee35
Caleb <89772184+belacmu@users.noreply.github.com>
2024-04-24 15:02:49 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
+const withBundleAnalyzer = import('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
   })
 
